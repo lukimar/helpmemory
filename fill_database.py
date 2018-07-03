f020451fc5790f6ff674be3c69449bd10d433413
lukimar <39347521+lukimar@users.noreply.github.com>
2018-07-03 10:56:00 +0200

    change names

---
+++ fill_database.py
@@ -0,0 +1,185 @@
+from tkinter import *
+import platform
+if platform.system()=='Windows':
+    windows=True
+else :
+    windows=False
+
+if windows:
+        import py_win_keyboard_layout
+
+def deNoise(text):
+    noise = re.compile("""   َ    | # Fatha
+                             ً    | # Tanwin Fath
+                             ُ    | # Damma
+                             ٌ    | # Tanwin Damm
+                             ِ    | # Kasra
+                             ٍ    | # Tanwin Kasr
+                             ْ    | # Sukun
+                             ـ     # Tatwil/Kashida
+                         """, re.VERBOSE)
+    text = re.sub(noise, '', text)
+    return text
+
+
+def callback():
+    writesyn=False
+    newsyn=False
+    categoria=var.get()
+
+    arabic_less=deNoise(entry_arab.get())
+    arabic=entry_arab.get()
+    if len(arabic.split())==1:
+        arabic+=" -"
+        arabic_less+=" -"
+    trad=entry_french.get()
+    inserzione=number.get()
+    if len(categoria)==0 or len(arabic)==0 or len(trad)==0 or len(inserzione)==0:
+        print('Error: Some Entries were not filled')
+        return;
+#    if not inserzione.isdigit():
+ #       print('Error: The entry "Inserzione numero:" can only accept numbers')
+ #       return;
+    arab_encoded=arabic.encode('utf-8')
+    less_encoded=arabic_less.encode('utf-8')
+    space=' '.encode('utf-8')
+    synonyms=0
+    synlist=[]
+    with open('synonyms.txt','ba') as synfile:
+        print()
+    with open('synonyms.txt','br') as synfile:
+        everysyn=synfile.readlines()
+        for i in range(len(everysyn)):
+            newline=everysyn[i]
+            fullist=newline.decode().split()
+            if trad==fullist[0]:
+                writesyn=True
+                everysyn[i]=newline+space+less_encoded
+    if writesyn:    
+        with open('synonyms.txt','bw') as synwrite:
+            synwrite.writelines(everysyn)
+    else:
+        with open('dictionary.txt','rb') as fichier:
+            for newline in fichier.readlines():
+                fullist=newline.split()
+                if trad==fullist[2].decode():
+                    newsyn=True
+    if newsyn:
+        with open('synonyms.txt','ab') as fichier:
+            print(len(everysyn))
+            if len(everysyn)!=0:
+                fichier.write('\n'.encode('utf-8'))
+            string=trad.encode('utf-8')+space+fullist[1]+space+less_encoded
+            fichier.write(string)
+    
+    with open('dictionary.txt','ab') as fichier:
+        fichier.write(arab_encoded)
+        fichier.write(space)
+        fichier.write(less_encoded)
+    with open('dictionary.txt','a') as fichier:
+        endstr=' '+trad+' '+categoria+' '+inserzione+'\n'
+        fichier.write(endstr)
+    entry_arab.delete(0,20)
+    entry_french.delete(0,20)
+
+
+    
+def arab():
+    py_win_keyboard_layout.change_foreground_window_keyboard_layout(-255851519)
+    return False
+def french():
+    py_win_keyboard_layout.change_foreground_window_keyboard_layout(layout)
+    return False
+
+def exiting():
+    py_win_keyboard_layout.change_foreground_window_keyboard_layout(layout)
+    master.destroy()
+
+def clear():
+    file=open('dictionary.txt','w')
+    file.close()
+    file=open('synonyms.txt','w')
+    file.close()
+    hide()
+
+def show():
+    pop_up.deiconify()
+
+def hide():
+    pop_up.withdraw()
+
+
+def last_line():
+    with open('dictionary.txt','rb') as fichier:
+        liste=fichier.readlines()
+        longueur=len(liste)
+        if longueur:
+            del liste[longueur-1]
+    with open('dictionary.txt','wb') as fichier:
+        fichier.writelines(liste)
+
+def call(event):
+    callback()
+    entry_arab.focus_set()
+
+category=('Verbo', 'Nome', 'Aggettivo', 'Avverbio');
+master = Tk()
+master.option_add("*Font","Calibri 22")
+var = StringVar()
+
+entry_arab = Entry(master,validate='focusin')
+entry_arab.grid(row=2,column=0,padx=10,pady=10)
+
+
+entry_french=Entry(master,validate='focusin')
+entry_french.grid(row=2,column=1,padx=10,pady=10)
+
+presentation=Label(master,text='Scrivere qua tutte le informazioni sulla parola :')
+presentation.grid(row=0,column=0,columnspan=4,padx=10,pady=10)
+
+arabo=Label(master,text='Arabo :')
+arabo.grid(row=1,column=0,padx=10,pady=10)
+
+ita=Label(master,text='Italiano :')
+ita.grid(row=1,column=1,padx=10,pady=10)
+
+grammatica=Spinbox(master,values=('Verbo','Nome','Aggettivo','Avverbio'),textvariable=var,state='readonly',wrap=True)
+grammatica.grid(row=4,column=0,padx=10,pady=10)
+gramm=Label(master,text='Categoria grammaticale')
+gramm.grid(row=3,column=0,padx=10,pady=10)
+number=StringVar()
+numero_en=Spinbox(master,increment=1,from_=1,to=1000,textvariable=number,state='readonly')
+numero_en.grid(row=4,column=1,padx=10,pady=10)
+insert=Label(master,text="Numero d'inserzione :")
+insert.grid(row=3,column=1,padx=10,pady=10)
+okay = Button(master, text="Okay", width=10, command=callback)
+okay.grid(row=5,column=1,padx=10,pady=10)
+
+empty=Button(master,text='Empty',command=show)
+empty.grid(row=5,column=0,padx=10,pady=10)
+
+pop_up=Toplevel()
+
+warning=Label(pop_up,text='The file <dictionary.txt> is going to be cleared',fg='red')
+warning.grid(row=0,column=0,columnspan=2,padx=10,pady=10)
+clear_but=Button(pop_up,text='Okay',command=clear,bg='red')
+clear_but.grid(row=1,column=1,padx=10,pady=10)
+
+back=Button(pop_up,text='Cancel',command=hide)
+back.grid(row=1,column=0,padx=10,pady=10)
+hide()
+
+remove=Button(master,text='Remove',command=last_line)
+remove.grid(row=5,column=0,columnspan=2,padx=10,pady=10)
+
+master.bind("<Return>",call)
+entry_arab.focus_set()
+if windows:
+    print('ok')
+    layout=py_win_keyboard_layout.get_foreground_window_keyboard_layout()
+    master.protocol("WM_DELETE_WINDOW",exiting)
+    entry_arab['validatecommand']=arab
+    entry_french['validatecommand']=french
+   
+master.mainloop()
+
---
+++ quiz.py
@@ -0,0 +1,317 @@
+from tkinter import *
+import platform
+if platform.system()=='Windows':
+    windows=True
+else :
+    windows=False
+print(windows)    
+from random import randint
+if windows:
+        import py_win_keyboard_layout
+
+with open('dictionary.txt','rb') as fichier:
+    print(fichier.read())
+
+
+fen=Tk()
+fen.option_add("*Font","Calibri 22")
+valid_list=[]
+syn_list=[]
+rand=0
+lang=0
+trad=0
+def disable_inserzione():
+    if state_inserzione.get()==0:
+        which_inserzione['state']='disabled';
+    else:
+        which_inserzione['state']='readonly';
+
+def search():
+    
+    global valid_list,rand,lang,trad
+    valid_list=[]
+    
+    with open('dictionary.txt','rb') as fichier:
+        
+        for newline in fichier.readlines():
+            newlist=newline.decode().split()
+            if categoria_var.get()=='Tutto' or categoria_var.get()==newlist[5]:
+                if state_inserzione.get()==0 or inserzione_var.get()==newlist[6]:
+                    valid_list.append(newlist)
+
+
+    if valid_list==[]:
+        print('There is no matching word in the dictionary file')
+        return
+    start_quiz()
+
+
+def lez_search():
+    global valid_list
+    labels=[]
+    valid_list=[]
+    i=0
+    with open('dictionary.txt','rb') as fichier:
+        for newline in fichier.readlines() :
+            listed=newline.decode().split()
+            if listed[6]==lezione_var.get() and i<=int(wordnumber_var.get()):
+                i+=1
+                valid_list.append(listed)
+    definitions=str()
+    message=Text(lezione,width=30,height=10)
+    for i in range(len(valid_list)):
+        #insert((i+1).0,valid_list[i][2])
+        #insert((i+1).(30-len(valid_list[i][0])),valid_list[i][0])
+        tradu=valid_list[i][2]+'     =     '+valid_list[i][0]+'\n'
+        definitions+=tradu
+
+    start_lezione()
+    message.insert(2.10,definitions)
+    message['state']='disabled'
+    message.grid(row=0,column=0,padx=10,pady=10)
+    scroll=Scrollbar(lezione,orient='vertical',command=message.yview)
+    scroll.grid(row=0,column=1,sticky='ns')
+    message['yscrollcommand']=scroll.set
+
+def trans(event):
+    translate()
+
+def newword():
+    global valid_list, rand,lang,trad,syn_list
+    entry_trad.delete(0,20)    
+    length=len(valid_list)-1
+    rand=randint(0,length)
+
+    string= valid_list[rand][lang] + " : "
+    trad_var.set(string)
+    syn_list=[]
+    if lingua_var.get()=="Italiano":
+        with open('synonyms.txt','rb') as fichier:
+            for newline in fichier.readlines():
+                print(newline)
+                fullist=newline.split()
+                print(fullist[0].decode())
+                print(valid_list[rand][2])
+                if fullist[0].decode()==valid_list[rand][2]:
+                    for i in range(len(fullist)-1):
+                        syn_list.append(fullist[i+1])
+    
+def translate():
+    global valid_list, rand,lang,trad,syn_list
+    traduc=entry_var.get()
+    if traduc=="":
+        info['fg']='black'
+        info_var.set('Il campo è vuoto')
+        return
+    if lingua_var.get()=="Arabo" and traduc==valid_list[rand][trad]:
+        info_var.set('Bravo!')
+        info['fg']='green'
+        
+    elif len(traduc.split())==1 and traduc==valid_list[rand][trad]:
+        if valid_list[rand][1]=="-":
+            info_var.set('Bravo!')
+            info['fg']='green'
+        else :
+            if valid_list[rand][5]=="Nome":
+                info_var.set('E il plurale?')
+            elif valid_list[rand][5]=="Verbo":
+                info_var.set('E il presente?')
+            info['fg']='orange'
+            return
+    elif len(traduc.split())==2:
+        if traduc.split()[0]==valid_list[rand][trad]:
+            if traduc.split()[1]==valid_list[rand][3]:
+                info_var.set('Bravo!')
+                info['fg']='green'
+            else :
+                if valid_list[rand][5]=="Nome":
+                    info_var.set('Il plurale\nè sbagliato')
+                elif valid_list[rand][5]=="Verbo":
+                    info_var.set('Il presente\nè sbagliato')
+                entry_trad.delete(0,20)
+                info['fg']='red'
+                return
+    else:
+        for synonyme in syn_list:
+            if traduc.split()[0]==synonyme.decode():
+                info_var.set('Un sinonimo?')
+                info['fg']='orange'
+                entry_trad.delete(0,20)
+                return
+    
+    if traduc.split()[0]!=valid_list[rand][trad]:
+        info_var.set('Riprova!')
+        info['fg']='red'
+        entry_trad.delete(0,20)
+        return
+
+    newword()
+
+def start_quiz():
+    global rand,lang,trad
+    menu.grid_remove()
+    para.grid_remove()
+    lezione.grid_remove()
+    cadre.grid()
+    entry_trad.bind("<Return>",trans)
+    entry_trad.focus_set()
+    if windows:
+        if lingua_var.get()=="Italiano":
+            arab()
+            lang=4
+            trad=2
+        else:
+            french()
+            lang=0
+            trad=4
+    newword()
+
+            
+def start_para():
+    menu.grid_remove()
+    cadre.grid_remove()
+    para.grid(row=0,column=0,padx=50,pady=50)
+
+def start_menu():
+    cadre.grid_remove()
+    para.grid_remove()
+    lezione_par.grid_remove()
+    lezione.grid_remove()
+    menu.grid(padx=50,pady=50)
+
+def start_parlezione():
+    menu.grid_remove()
+    lezione_par.grid(padx=50,pady=50)
+
+def start_lezione():
+    lezione_par.grid_remove()
+    lezione.grid()
+    
+def get_solution():
+    global rand,valid_list,trad
+    string=valid_list[rand][trad]
+    if valid_list[rand][3]!="-" and lingua_var.get()=="Italiano":
+        string+=" "+valid_list[rand][3]
+    entry_var.set(string)
+
+def arab():
+    py_win_keyboard_layout.change_foreground_window_keyboard_layout(-255851519)
+    return False
+def french():
+    py_win_keyboard_layout.change_foreground_window_keyboard_layout(layout)
+    return False
+
+def exiting():
+    py_win_keyboard_layout.change_foreground_window_keyboard_layout(layout)
+    fen.destroy()
+    
+def disable_wordnumber():
+    if state_wordnumber.get()==0:
+        which_wordnumber['state']='disabled';
+    else:
+        which_wordnumber['state']='readonly';
+
+fen.rowconfigure(0,minsize=800)
+fen.columnconfigure(0,minsize=1000)
+
+menu=LabelFrame(fen,text='Menu')
+menu.grid(row=0,column=0)
+
+lezione_but=Button(menu,text='Lezione',command=start_parlezione)
+lezione_but.grid(column=0,row=1,padx=50,pady=50)
+
+quiz=Button(menu,text='Quiz',command=start_para)
+quiz.grid(column=0,row=0,padx=50,pady=50)
+
+but_quit=Button(menu,text='Uscire',command=exiting)
+but_quit.grid(column=0,row=2,padx=50,pady=50)
+
+para=LabelFrame(fen,text='Parametri')
+
+
+lezione_par=LabelFrame(fen,text='Parametri')
+
+lez_insert=Label(lezione_par,text="Lezione numero :")
+lez_insert.grid(padx=10,pady=10,row=0,column=0)
+lezione_var=StringVar()
+which_lezione=Spinbox(lezione_par,increment=1,from_=1,to=1000,textvariable=lezione_var,state='readonly')
+which_lezione.grid(padx=10,pady=10,row=1,column=0,columnspan=1)
+
+state_wordnumber=IntVar()
+word_number=Checkbutton(lezione_par,text="Numero di parole :",variable=state_wordnumber,command=disable_wordnumber)
+word_number.grid(row=0,column=1)
+wordnumber_var=StringVar()
+which_wordnumber=Spinbox(lezione_par,increment=1,state='disabled',from_=10,to=1000,textvariable=wordnumber_var)
+which_wordnumber.grid(row=1,column=1)
+
+okay_lez=Button(lezione_par,text='Okay',command=lez_search)
+okay_lez.grid(row=2,column=1)
+
+backmenu_lez=Button(lezione_par,text='Menu',command=start_menu)
+backmenu_lez.grid(row=2,column=0)
+lezione=LabelFrame(fen,text='Lezione')
+
+
+quiz_lez=Button(lezione,text='Quiz',command=start_quiz)
+quiz_lez.grid(row=0,column=2,padx=10,pady=10)
+
+cadre=LabelFrame(fen,text='Quiz')
+cadre.columnconfigure(1,minsize=300)
+
+
+lingua=Label(para,text="Lingua iniziale :")
+lingua.grid(padx=10,pady=10,row=0,column=0)
+lingua_var=StringVar()
+which_lingua=Spinbox(para,values=("Italiano","Arabo"),textvariable=lingua_var,state='readonly',wrap=True)
+which_lingua.grid(padx=10,pady=10,row=1,column=0)
+
+categoria=Label(para,text='Categoria grammaticale :')
+categoria.grid(padx=10,pady=10,row=0,column=1)
+categoria_var=StringVar()
+which_categoria=Spinbox(para,values=('Tutto','Verbo','Nome','Aggettivo','Avverbio'),textvariable=categoria_var,state='readonly',wrap=True)
+which_categoria.grid(padx=10,pady=10,row=1,column=1)
+
+state_inserzione=IntVar()
+inserzione=Checkbutton(para,text="Scegliete un'inserzione",variable=state_inserzione,command=disable_inserzione)
+inserzione.grid(padx=10,pady=10,row=2,column=0,columnspan=2)
+inserzione_var=StringVar()
+which_inserzione=Spinbox(para,increment=1,from_=1,to=1000,textvariable=inserzione_var,state='disabled')
+which_inserzione.grid(padx=10,pady=10,row=3,column=0,columnspan=2)
+
+okay=Button(para,text='Okay',command=search)
+okay.grid(padx=50,pady=50,column=1,row=4)
+
+anuller=Button(para,text='Menu',command=start_menu)
+anuller.grid(padx=50,pady=50,column=0,row=4)
+
+start=Button(cadre,command=translate,text='Continuare')
+start.grid(padx=30,pady=30,column=2,row=7)
+
+
+solution=Button(cadre,text='Soluzione',command=get_solution)
+solution.grid(padx=30,pady=30,column=2,row=6)
+info_var=StringVar()
+info=Label(cadre,textvariable=info_var)
+info.grid(padx=30,pady=30,row=6,column=1,rowspan=2)
+info_var.set('Traduzione?')           
+trad_var=StringVar()
+traduction=Label(cadre,textvariable=trad_var)
+traduction.grid(padx=30,pady=30,column=0,columnspan=3,row=4)
+
+entry_var=StringVar()
+entry_trad=Entry(cadre,textvariable=entry_var)
+entry_trad.grid(padx=30,pady=30,row=5,column=0,columnspan=3)
+
+para_back=Button(cadre,text='Parametri',command=start_para)
+para_back.grid(padx=30,pady=30,row=6,column=0)
+
+menu_back=Button(cadre,text='Menu',command=start_menu)
+menu_back.grid(padx=30,pady=30,row=7,column=0)
+
+if windows:
+    fen.protocol("WM_DELETE_WINDOW",exiting)
+    layout=py_win_keyboard_layout.get_foreground_window_keyboard_layout()
+
+fen.mainloop()
+
+
